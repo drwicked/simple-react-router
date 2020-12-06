de002b44fe03d89a94662433fdb15ec36949b737
Jeff P <yeffrey@gmail.com>
2020-12-05 21:45:41 -0600

    add lint

--- src/ButtonLink.js
+++ src/ButtonLink.js
@@ -1,5 +1,14 @@
-import {Link} from './Link';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from './Link';
 
-export default function ButtonLink(props){
-  return <Link Component="button" {...props} />{props.children}</Link>
+function ButtonLink(props) {
+  const { children } = props;
+  return <Link Component="button" {...props}>{ children }</Link>
 }
+
+ButtonLink.propTypes = {
+  children: PropTypes.element.isRequired,
+}
+
+export default ButtonLink;
